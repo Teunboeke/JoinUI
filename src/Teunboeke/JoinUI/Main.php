cb21126bbc7a7de17fc42ecb60024840f621fbcc
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2021-01-21 17:24:40 +0100

    Create Main.php

---
+++ src/Teunboeke/JoinUI/Main.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Teunboeke\JoinUI;
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\player\PlayerJoinEvent;
+use pocketmine\form\Form;
+
+class Main extends PluginBase implements Listener, form {
+
+   public $data = [];
+
+   public function onEnable(){
+     $this->getServer()->getPluginManager()->registerEvents($this, $this);
+   }
+   
+   public function onJoin(PlayerJoinEvent $event){
+   
+     $player = $event->getPlayer();
+     $name = $player->getName();
+     $this->openMyForm($sender);
+   }
+   break;
+ }
+ 
+ return true;
+}
+
+    public function openMyForm(Player $player){
+     
+     $this->data = [];
+     $this->data["type"] = "form";
+     $this->data["title"] = "Welcome to CloudNetwork";
+     $this->data["content"] = "Welcome to CloudNetwork this is a test";
+     $this>data["buttons"] = [];
+     $player->sendForm($this);
+    }
+    
+    public function handleResponse(Player $player, $data); void
+    {
+    
+    }
+    
+    public function jsonSerialize()
+    {
+      return $this->data;
+    }

0edd97861caf110278ba93e7cc09d058857521d9
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2021-01-21 20:14:01 +0100

    Fix

--- src/Teunboeke/JoinUI/Main.php
+++ src/Teunboeke/JoinUI/Main.php
@@ -23,11 +23,8 @@ class Main extends PluginBase implements Listener, form {
      $this->openMyForm($sender);
    }
  }
- 
- return true;
-}
 
-public function openMyForm(Player $player){
+     public function openMyForm(Player $player){
      
      $this->data = [];
      $this->data["type"] = "form";

a0a361f64bc290cbfdf88af8c1ddd0c281a06024
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2021-01-24 17:01:21 +0100

    Update Main.php

--- src/Teunboeke/JoinUI/Main.php
+++ src/Teunboeke/JoinUI/Main.php
@@ -28,11 +28,9 @@ class Main extends PluginBase implements Listener {
    public function openMyForm(Player $player){
      
      $this->data = [];
-     $this->data["type"] = "form";
-      
+     $this->data["type"] = "form";     
      $this->data["title"] = "Welcome to CloudNetwork";
-     $this->data["content"] = "Welcome to CloudNetwork this is a test";
-      
+     $this->data["content"] = "Welcome to CloudNetwork this is a test";      
      $this->data["buttons"] = [];
      $player->sendForm($this);
    }
